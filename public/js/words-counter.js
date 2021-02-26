456dd756e0ad33e434b2b0bb01665aefb0809639
a-deep97 <a.deep97@outlook.com>
2021-02-26 21:04:07 +0530

    counting definition changed for special chars

--- public/js/words-counter.js
+++ public/js/words-counter.js
@@ -1,5 +1,8 @@
 
 
+/*
+Removes the extra spaces between two words
+*/
 function removeExtraSpace(para){
     para = para.replace(/(^\s*)|(\s*$)/gi,"");
 	para = para.replace(/[ ]{2,}/gi," ");
@@ -7,7 +10,34 @@ function removeExtraSpace(para){
     return para;
 }
 
+/*
+finds if a word is just a special character.
+    (if a word has only special chars then 
+     it is considered as special word)
+*/
+function isSpecialChar(word){
+    
+    var special=/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    for(var i=0;i<word.length;i++){
+        if(!special.test(word[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+/*
+calculates the words in the para 
+*/
 function counter(para){
     para=removeExtraSpace(para);
-    return para.split(" ").length;
+    para = para.split(" ");
+    var result=0;
+    for(var i=0;i<para.length;i++){
+        if(!isSpecialChar(para[i])){
+            result+=1;
+            console.log(result);
+        }
+    }
+    return result;
 }
