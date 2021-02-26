056f94062a50ed1933e66bdaeace728cd7baf3e7
aman deep <a.deep97@outlook.com>
2021-02-26 23:53:52 +0530

    Create words-counter.js

---
+++ js/words-counter.js
@@ -0,0 +1,49 @@
+
+
+/*
+Removes the extra spaces between two words
+*/
+function removeExtraSpace(para){
+    para = para.replace(/(^\s*)|(\s*$)/gi,"");
+	para = para.replace(/[ ]{2,}/gi," ");
+    para = para.replace(/\n /,"\n");
+    return para;
+}
+
+/*
+finds if a word is just a special character.
+    (if a word has only special chars then 
+     it is considered as special word)
+*/
+function isSpecialChar(word){
+    
+    var special=/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    for(var i=0;i<word.length;i++){
+        if(!special.test(word[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+/*
+calculates the words in the para 
+*/
+function counter(para){
+    para=removeExtraSpace(para);
+    para = para.split(" ");
+    var words=0;
+    var chars=0;
+    var result;
+    for(var i=0;i<para.length;i++){
+        if(!isSpecialChar(para[i])){
+            words+=1;
+            chars+=para[i].length;
+        }
+        else{
+            chars+=1;
+        }
+    }
+    result={words,chars};
+    return result;
+}
